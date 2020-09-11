dca2f3a1c691d13c6c9732ae355154b25a9a1b28
himani88jain <himani7.88jain@gmail.com>
2020-09-11 12:56:28 -0400

    chnages in user file

--- src/main/java/co/grandcircus/Final_Project/entity/User.java
+++ src/main/java/co/grandcircus/Final_Project/entity/User.java
@@ -63,9 +63,61 @@ public class User {
 		this.password = password;
 	}
 
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Double getHeight() {
+		return height;
+	}
+
+	public void setHeight(Double height) {
+		this.height = height;
+	}
+
+	public Double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getActivityLevel() {
+		return activityLevel;
+	}
+
+	public void setActivityLevel(String activityLevel) {
+		this.activityLevel = activityLevel;
+	}
+
+	public Integer getShoppingInterval() {
+		return shoppingInterval;
+	}
+
+	public void setShoppingInterval(Integer shoppingInterval) {
+		this.shoppingInterval = shoppingInterval;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + "]";
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", gender="
+				+ gender + ", height=" + height + ", weight=" + weight + ", age=" + age + ", activityLevel="
+				+ activityLevel + ", shoppingInterval=" + shoppingInterval + "]";
 	}
+
+	
 	
 }
